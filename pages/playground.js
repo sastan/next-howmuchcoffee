7f8d02c3faf1e82affac63f91a0b5eeb83798cf9
Amadeus <38767929+Mozart409@users.noreply.github.com>
2020-11-29 10:37:53 +0100

    wired stuff

--- pages/playground.js
+++ pages/playground.js
@@ -1,12 +1,27 @@
 import { fetcherPaths } from "@gqlClient";
 
-const Playground = () => {
-  return <div>Playground</div>;
+const fetcher = (url) => fetch(url).then((r) => r.json());
+const Playground = (props) => {
+  return (
+    <div>
+      Playground
+      <span>{props.user}</span>
+      {/*    {data &&
+        data.map((node) => (
+          <div>
+            {node.ref.ts}
+           
+          </div>
+        ))} */}
+      {console.log(data)}
+    </div>
+  );
 };
 
 export async function getStaticProps(context) {
+  const user = fetcher("/api/user");
   return {
-    props: {},
+    props: { user },
   };
 }
 
