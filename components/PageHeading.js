a0178f2367d538ea720fbfe78b6e8b1aea42eeb8
Amadeus <38767929+Mozart409@users.noreply.github.com>
2020-12-16 14:12:32 +0100

    added head to PageHeading Component

--- components/PageHeading.js
+++ components/PageHeading.js
@@ -1,14 +1,17 @@
-
-import PropTypes from "prop-types";
-import cx from "classnames";
+import Head from 'next/head'
+import PropTypes from 'prop-types'
+import cx from 'classnames'
 function PageHeading(props) {
   return (
     <div className="md:flex md:items-center md:justify-between">
+      <Head>
+        <title>{props.title}</title>
+      </Head>
       <div className="flex-1 min-w-0">
         <h1
           className={cx(
-            "text-2xl font-bold leading-7 mt-4 text-gray-900 sm:text-3xl sm:leading-9",
-            { "text-gray-900 underline": props.underline }
+            'text-2xl font-bold leading-7 mt-4 text-gray-900 sm:text-3xl sm:leading-9',
+            { 'text-gray-900 underline': props.underline }
           )}
         >
           {props.title}
@@ -16,11 +19,11 @@ function PageHeading(props) {
         <h2>{props.subtitle}</h2>
       </div>
     </div>
-  );
+  )
 }
 
 PageHeading.propTypes = {
   title: PropTypes.string.isRequired,
   subtitle: PropTypes.string,
-};
-export default PageHeading;
+}
+export default PageHeading
