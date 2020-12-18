61ba623a62b7eab2da0d94c63487031e3c7cb2cf
Amadeus <38767929+Mozart409@users.noreply.github.com>
2020-12-18 18:17:59 +0100

    remove hamburger button

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,26 @@
 import Link from 'next/link'
 import { useState } from 'react'
 
-function Header () {
+function Header() {
   const [isExpanded, toggleExpansion] = useState(false)
 
   return (
     <header>
-      <div className='flex flex-wrap justify-between items-center p-4 mx-auto max-w-4xl md:flex-no-wrap md:p-8'>
-        <div className='flex items-center'>
+      <div className="flex flex-wrap justify-between items-center p-4 mx-auto max-w-4xl md:flex-no-wrap md:p-8">
+        <div className="flex items-center">
           <img
-            src='https://nextjs.org/static/favicon/apple-touch-icon.png'
-            className='mr-3 w-10 rounded text-yellow-custom'
+            src="https://nextjs.org/static/favicon/apple-touch-icon.png"
+            className="mr-3 w-10 rounded text-yellow-custom"
           />
 
-          <Link href='/'>
-            <a className='text-xl font-light text-gray-600 dark:text-gray-300'>
+          <Link href="/">
+            <a className="text-xl font-light text-gray-600 dark:text-gray-300">
               How Much Coffee
             </a>
           </Link>
         </div>
 
-        <button
+        {/* <button
           className='flex items-center py-2 px-3 rounded border border-gray-800 text-yellow-custom md:hidden'
           onClick={() => toggleExpansion(!isExpanded)}
         >
@@ -32,7 +32,7 @@ function Header () {
             <title>Menu</title>
             <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' />
           </svg>
-        </button>
+        </button> */}
 
         {/* <ul
           className={`${
